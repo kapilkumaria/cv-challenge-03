fa2082ba99c4691fb0096ab4fa8b5fe1f4c41654
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-12-18 23:23:40 +0000

    Fixes

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -34,11 +34,7 @@ variable "ingress_rules" {
       from_port          = 22,
       to_port            = 22,
       protocol           = "tcp",
-<<<<<<< HEAD
       cidr_blocks        = ["44.195.82.101/32"],
-=======
-      cidr_blocks        = ["3.239.200.35/32"],
->>>>>>> origin/infra_main
       ipv6_cidr_blocks   = [],
       security_group_ids = []
     },
