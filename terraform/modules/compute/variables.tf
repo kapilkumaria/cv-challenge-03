95a2208289550cef35e2dde15dfd62c08f55521b
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-12-18 21:53:26 +0000

    UpdaAllowed ec2 IP for security group

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -34,7 +34,7 @@ variable "ingress_rules" {
       from_port          = 22,
       to_port            = 22,
       protocol           = "tcp",
-      cidr_blocks        = ["3.234.207.199/32"],
+      cidr_blocks        = ["44.195.82.101/32"],
       ipv6_cidr_blocks   = [],
       security_group_ids = []
     },
