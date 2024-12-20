0d7d7ca12f065adab64c28415402f2c61ce9c594
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-12-20 06:57:31 +0000

    Deleted terraform-apply.yml.bkp

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -119,7 +119,7 @@ resource "aws_route53_record" "root_record" {
 
 resource "null_resource" "run_ansible" {
   provisioner "local-exec" {
-    command = "ansible-playbook -i ansible/inventory/ansible.ini ansible/site.yml -vvvv"
+    command = "ansible-playbook -i ../ansible/inventory/ansible.ini ../ansible/site.yml -vvvv"
   }
 
   depends_on = [
